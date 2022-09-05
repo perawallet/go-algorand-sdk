91b17f8c6e751af3f9c32a76990376b2e2fad469
Ömer Aslan <omer@hipo.biz>
2022-09-05 13:45:18 +0300

    :white_check_mark: Add more tests
    Enhance error handling

--- mobile/encryption.go
+++ mobile/encryption.go
@@ -2,64 +2,86 @@ package mobile
 
 import (
 	"io"
-	"fmt"
 	"crypto/rand"
 	"golang.org/x/crypto/nacl/secretbox"
 )
 
 
-type EncryptionError struct {
-	code int
+type Encryption struct {
+	EncryptedData []byte
+	DecryptedData []byte
+	ErrorCode int
 
+	// ErrorCode Descriptions
+	// 0 => No Error
 	// 1 => Invalid SecretKey
 	// 2 => Random Generator Error
 	// 3 => Invalid encrypted data length
 	// 4 => Decryption error
 }
 
-func (e *EncryptionError) Error() string {
-    return fmt.Sprintf("%d", e.code)
-}
-
-func Encrypt(data []byte, sk []byte) ([]byte, error)  {
+func Encrypt(data []byte, sk []byte) Encryption  {
 	var secretKey [32]byte
 
 	if len(sk) != len(secretKey) {
-		return nil, &EncryptionError{1}
+		return Encryption{
+			ErrorCode: 1,
+		}
 	}
 
 	copy(secretKey[:], sk)
 
 	var nonce [24]byte
 	if _, err := io.ReadFull(rand.Reader, nonce[:]); err != nil {
-		return nil, &EncryptionError{2}
+		return Encryption{
+			ErrorCode: 2,
+		}
 	}
 
 	encrypted := secretbox.Seal(nonce[:], data, &nonce, &secretKey)
 
-	return encrypted, nil
+	encryptedData := Encryption{
+		EncryptedData: encrypted,
+		ErrorCode: 0,
+	}
+
+	return encryptedData
 }
 
-func Decrypt(data []byte, sk []byte) ([]byte, error) {
+func Decrypt(data []byte, sk []byte) Encryption {
 	var secretKey [32]byte
 
 	if len(sk) != len(secretKey) {
-		return nil, &EncryptionError{1}
+		return Encryption{
+			EncryptedData: data,
+			ErrorCode: 1,
+		}
 	}
 
 	copy(secretKey[:], sk)
 
 	if len(data) < 24 {
-		return nil, &EncryptionError{3}
+		return Encryption{
+			EncryptedData: data,
+			ErrorCode: 3,
+		}
 	}
 
 	var decryptNonce [24]byte
 	copy(decryptNonce[:], data[:24])
 
 	decrypted, ok := secretbox.Open(nil, data[24:], &decryptNonce, &secretKey)
+	
 	if !ok {
-		return nil, &EncryptionError{4}
+		return Encryption{
+			EncryptedData: data,
+			ErrorCode: 4,
+		}
 	}
 
-	return decrypted, nil
+	return Encryption{
+		EncryptedData: data,
+		DecryptedData: decrypted,
+		ErrorCode: 0,
+	}
 }
--- mobile/encryption_test.go
+++ mobile/encryption_test.go
@@ -2,55 +2,117 @@ package mobile
 
 import (
 	"testing"
-	"io"
-	"crypto/rand"
-
 	"github.com/stretchr/testify/require"
-	"golang.org/x/crypto/nacl/secretbox"
 )
 
-func TestSecretboxEncrypt(t *testing.T) {
-	test_string := "testdata"
-	test_data := []byte(test_string)
-	secret_key := "0123456789ABCDEFGHIJKLMNOPQRSTUV"
+const (
+	firstValidSecretKey = "0123456789ABCDEFGHIJKLMNOPQRSTUV"
+	firstInvalidSecretKey = "0123456789AB"
+	secondValidSecretKey = "9876456789ABCDEFGAIJKKMNDPQSSTCV"
+)
 
-	require.Len(t, secret_key, 32)
 
-	var secret_data [32]byte
-	copy(secret_data[:], secret_key)
+func TestSecretboxEncryption(t *testing.T) {
+	testString := "testdata"
+	testData := []byte(testString)
+	secretKey := firstValidSecretKey
+	secretData := []byte(secretKey)
 
-	var nonce [24]byte
-	_, err := io.ReadFull(rand.Reader, nonce[:])
+	encryptedContent := Encrypt(testData, secretData)
 
-	require.NoError(t, err)
+	require.NotEmpty(t, encryptedContent)
+	require.NotEmpty(t, encryptedContent.EncryptedData)
+	require.Empty(t, encryptedContent.DecryptedData)
+	require.Equal(t, encryptedContent.ErrorCode, 0)
 
-	encrypted := secretbox.Seal(nonce[:], test_data, &nonce, &secret_data)
+	decryptedContent := Decrypt(encryptedContent.EncryptedData, secretData)
 
-	require.NotEmpty(t, encrypted)
+	require.NotEmpty(t, decryptedContent)
+	require.NotEmpty(t, decryptedContent.DecryptedData)
+	require.NotEmpty(t, decryptedContent.EncryptedData)
+	require.Equal(t, decryptedContent.ErrorCode, 0)
+	require.Equal(t, decryptedContent.DecryptedData, testData)
+	require.Equal(t, decryptedContent.EncryptedData, encryptedContent.EncryptedData)
 }
 
-func TestSecretboxDecrypt(t *testing.T) {
-	test_string := "testdata"
-	test_data := []byte(test_string)
-	secret_key := "0123456789ABCDEFGHIJKLMNOPQRSTUV"
-	var secret_data [32]byte
-	copy(secret_data[:], secret_key)
+func TestSecretboxInvalidSecretKey(t *testing.T) {
+	testString := "testdata"
+	testData := []byte(testString)
+	secretKey := firstInvalidSecretKey
+	secretData := []byte(secretKey)
 
-	var nonce [24]byte
-	io.ReadFull(rand.Reader, nonce[:])
+	encryptedContent := Encrypt(testData, secretData)
 
-	encrypted := secretbox.Seal(nonce[:], test_data, &nonce, &secret_data)
+	require.NotEmpty(t, encryptedContent)
+	require.Equal(t, encryptedContent.ErrorCode, 1)
+	require.Empty(t, encryptedContent.EncryptedData)
+	require.Empty(t, encryptedContent.DecryptedData)
 
-	lenData := len(encrypted)
+	decryptedContent := Decrypt(encryptedContent.EncryptedData, secretData)
 
-	require.True(t, lenData > 24)
+	require.NotEmpty(t, decryptedContent)
+	require.Empty(t, encryptedContent.EncryptedData)
+	require.Empty(t, encryptedContent.DecryptedData)
+	require.Equal(t, encryptedContent.ErrorCode, 1)
+}
 
-	var decryptNonce [24]byte
-	copy(decryptNonce[:], encrypted[:24])
+func TestSecretboxSameDataWithDifferentEncryptions(t *testing.T) {
+	testString := "testdata"
+	testData := []byte(testString)
+	secretKey := firstValidSecretKey
+	secretData := []byte(secretKey)
 
-	decrypted, ok := secretbox.Open(nil, encrypted[24:], &decryptNonce, &secret_data)
-	
-	require.NotEmpty(t, decrypted)
-	require.True(t, ok)
-	require.Equal(t, decrypted, test_data)
+	firstEncryptedContent := Encrypt(testData, secretData)
+
+	require.NotEmpty(t, firstEncryptedContent)
+	require.NotEmpty(t, firstEncryptedContent.EncryptedData)
+	require.Empty(t, firstEncryptedContent.DecryptedData)
+	require.Equal(t, firstEncryptedContent.ErrorCode, 0)
+
+	secondSecretKey := secondValidSecretKey
+	secondSecretData := []byte(secondSecretKey)
+
+	secondEncryptedContent := Encrypt(testData, secondSecretData)
+
+	require.NotEmpty(t, secondEncryptedContent)
+	require.NotEmpty(t, secondEncryptedContent.EncryptedData)
+	require.Empty(t, secondEncryptedContent.DecryptedData)
+	require.Equal(t, secondEncryptedContent.ErrorCode, 0)
+
+	firstDecryptedContent := Decrypt(firstEncryptedContent.EncryptedData, secretData)
+
+	require.NotEmpty(t, firstDecryptedContent)
+	require.NotEmpty(t, firstDecryptedContent.DecryptedData)
+	require.NotEmpty(t, firstDecryptedContent.EncryptedData)
+	require.Equal(t, firstDecryptedContent.ErrorCode, 0)
+	require.Equal(t, firstDecryptedContent.DecryptedData, testData)
+	require.Equal(t, firstDecryptedContent.EncryptedData, firstEncryptedContent.EncryptedData)
+
+	secondDecryptedContent := Decrypt(secondEncryptedContent.EncryptedData, secondSecretData)
+
+	require.NotEmpty(t, secondDecryptedContent)
+	require.NotEmpty(t, secondDecryptedContent.DecryptedData)
+	require.NotEmpty(t, secondDecryptedContent.EncryptedData)
+	require.Equal(t, secondDecryptedContent.ErrorCode, 0)
+	require.Equal(t, secondDecryptedContent.DecryptedData, testData)
+	require.Equal(t, secondDecryptedContent.DecryptedData, firstDecryptedContent.DecryptedData)
+	require.Equal(t, secondDecryptedContent.EncryptedData, secondDecryptedContent.EncryptedData)
+
+	secondDecryptedContentWithFirstSecret := Decrypt(secondEncryptedContent.EncryptedData, secretData)
+
+	require.NotEmpty(t, secondDecryptedContentWithFirstSecret)
+	require.Equal(t, secondDecryptedContentWithFirstSecret.ErrorCode, 4)
+	require.NotEmpty(t, secondDecryptedContentWithFirstSecret.EncryptedData)
+	require.Empty(t, secondDecryptedContentWithFirstSecret.DecryptedData)
+
+	var extractedContent []byte
+	copy(extractedContent[:], secondEncryptedContent.EncryptedData[:24])
+
+	invalidDecryptedContent := Decrypt(extractedContent, secretData)
+
+	require.NotEmpty(t, invalidDecryptedContent)
+	require.Empty(t, invalidDecryptedContent.DecryptedData)
+	require.Empty(t, invalidDecryptedContent.EncryptedData)
+	require.NotEqual(t, invalidDecryptedContent.EncryptedData, secondEncryptedContent.EncryptedData)
+	require.Equal(t, invalidDecryptedContent.ErrorCode, 3)
 }
